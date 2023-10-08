95ae940d28ad74b423902f2779e2999545f33ace
Mike Lindner <mikelindner@mail.com>
2023-10-08 14:10:29 +1030

    incremented date in footer

--- footer.php
+++ footer.php
@@ -2,12 +2,12 @@
 <br>
 <br>
 <div id= "ttr_footer">
-    <a href="https://photography.mikelindner.com/">Photography</a> | 
-    <a href="https://computing.mikelindner.com/">Computing</a> | 
-    <a href="https://cooking.mikelindner.com/">Cooking</a> | 
-    <a href="https://mikelindner.com/index.php/contact/">Contact</a> | 
+    <a href="https://photography.mikelindner.com/">Photography</a> |
+    <a href="https://computing.mikelindner.com/">Computing</a> |
+    <a href="https://cooking.mikelindner.com/">Cooking</a> |
+    <a href="https://mikelindner.com/index.php/contact/">Contact</a> |
     <br>
-    <a href="">(c) 2008-2022 Michael Lindner</a>
+    <a href="">(c) 2008-2023 Michael Lindner</a>
 </div>
 </div>
 
