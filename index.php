469d5c26388db2c63d2e205c602e5a4e94d38887
Mike Lindner <mikelindner@mail.com>
2025-04-26 14:52:22 +0930

    removed strip_tags

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ function get_first_image_from_post($post_content) {
 
 // Function to truncate content while preserving HTML tags
 function custom_truncate_content($content, $limit) {
-    $content = strip_tags($content); // Remove unwanted HTML tags
+//    $content = strip_tags($content); // Remove unwanted HTML tags
     $content = substr($content, 0, $limit);
     $content = rtrim($content, " \t\n\r\0\x0B.,;"); // Remove trailing characters to ensure valid HTML
 
