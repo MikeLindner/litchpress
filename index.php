71e725ac2bf0021f48717ff94f89bcfc1503b7dc
Mike Lindner <mikelindner@mail.com>
2025-04-26 18:33:50 +0930

    allowed tags array

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<?php get_header(); 
+<?php get_header();
 
 const POST_SIZE = 700;
 
 
 // Function to get the first image from post content
-function get_first_image_from_post($post_content) {
+function get_first_image_from_post($post_content)
+{
     $pattern = '/<img.*?src=["\'](.*?)["\']/i';
     preg_match($pattern, $post_content, $matches);
 
@@ -16,15 +17,16 @@ function get_first_image_from_post($post_content) {
 }
 
 
-   $allowed_tags = array('a', 'img', 'br', 'p', 'strong', 'em', 'video'); // add more tags as needed
 
 // Function to truncate content while preserving HTML tags
-function custom_truncate_content($content, $limit) {
+function custom_truncate_content($content, $limit)
+{
+    $allowed_tags = array('a', 'img', 'br', 'p', 'strong', 'em', 'video'); // add more tags as needed
     $content = strip_tags($content, $allowed_tags); // Remove unwanted HTML tags
     $content = substr($content, 0, $limit);
     $content = rtrim($content, " \t\n\r\0\x0B.,;"); // Remove trailing characters to ensure valid HTML
 
-//  If the content was truncated, add a "Read more" link
+    //  If the content was truncated, add a "Read more" link
     if (strlen($content) >= POST_SIZE) {
         $content .= '... <a href="' . get_permalink() . '"><br>Read more...</a>';
     }
@@ -33,40 +35,42 @@ function custom_truncate_content($content, $limit) {
 
 ?>
 <style>
-    #ttr_content a:hover{
+    #ttr_content a:hover {
         color: white;
     }
 </style>
 <div id="ttr_main" class="row">
-<div id="ttr_content" class="col-lg-12 col-sm-12 col-md-8 col-xs-12">
-    <div class="row">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-			<h1><a href="<?= get_permalink() ?>"><?php the_title(); ?></a></h1>
-			<b><?php the_time('F jS, Y') ?></b>
-			<br><br>
-			<?php
-			$thumbnail_url = get_first_image_from_post(get_the_content());
-			if ($thumbnail_url) :
-			?>
-				<div class="post-thumbnail">
-					<img src="<?php echo esc_url($thumbnail_url); ?>" alt="<?php the_title_attribute(); ?>">
-				</div>
-			<?php endif; ?>
-			<br>
-			<?php
-				$limited_content = custom_truncate_content(get_the_content(), POST_SIZE); // Change 500 to your desired number of characters
-				echo $limited_content;
-			?>
-			<br>
-        </div>
+    <div id="ttr_content" class="col-lg-12 col-sm-12 col-md-8 col-xs-12">
+        <div class="row">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <h1><a href="<?= get_permalink() ?>"><?php the_title(); ?></a></h1>
+                        <b><?php the_time('F jS, Y') ?></b>
+                        <br><br>
+                        <?php
+                        $thumbnail_url = get_first_image_from_post(get_the_content());
+                        if ($thumbnail_url) :
+                        ?>
+                            <div class="post-thumbnail">
+                                <img src="<?php echo esc_url($thumbnail_url); ?>" alt="<?php the_title_attribute(); ?>">
+                            </div>
+                        <?php endif; ?>
+                        <br>
+                        <?php
+                        $limited_content = custom_truncate_content(get_the_content(), POST_SIZE); // Change 500 to your desired number of characters
+                        echo $limited_content;
+                        ?>
+                        <br>
+                    </div>
 
-        <?php endwhile; else: ?>
-        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-        <?php endif; ?>
+                <?php endwhile;
+            else: ?>
+                <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+            <?php endif; ?>
+        </div>
     </div>
-</div>
-<?php //get_sidebar(); ?>
+    <?php //get_sidebar(); 
+    ?>
 </div>
 
 <?php get_footer(); ?>
@@ -77,5 +81,3 @@ sm (for tablets - screens equal to or greater than 768px wide)
 md (for small laptops - screens equal to or greater than 992px wide)
 lg (for laptops and desktops - screens equal to or greater than 1200px wide)
 -->
-
-
