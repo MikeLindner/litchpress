6538d571cbf49f3cac4914fab2021883bca9fc86
Mike Lindner <mikelindner@mail.com>
2026-02-01 23:17:06 +1030

    Widen book widget and add camo background

--- sidebar.php
+++ sidebar.php
@@ -4,6 +4,6 @@
     <h2 ><?php _e('Archives'); ?></h2>
     <ul > <?php wp_get_archives(); ?> </ul>
 
-    <iframe width='220' height='400' src='https://leanpub.com/CloudBook/embed' frameborder='0' allowtransparency='true'></iframe>
+    <iframe width='280' height='400' src='https://leanpub.com/CloudBook/embed' frameborder='0' allowtransparency='true'></iframe>
   
 </div>
