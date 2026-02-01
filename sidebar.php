c04cc70f6476645a6f54a1d8f77e1f0654d527a3
Mike Lindner <mikelindner@mail.com>
2026-02-01 22:39:46 +1030

    Remove redundant Categories title from sidebar widget

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <div id="ttr_sidebar" class="col-lg-2 col-md-2 col-sm-4 col-xs-12">
     <h2 ><?php _e('Categories'); ?></h2>
-    <ul > <?php wp_list_categories(['orderby' => 'name']); ?> </ul>
+    <ul > <?php wp_list_categories(['orderby' => 'name', 'title_li' => '']); ?> </ul>
     <h2 ><?php _e('Archives'); ?></h2>
     <ul > <?php wp_get_archives(); ?> </ul>
 
