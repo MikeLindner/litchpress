6d42f81b1c03106295faf83b9c9c5cb8f78f9c15
Mike Lindner <mikelindner@mail.com>
2026-02-01 23:46:04 +1030

    Extend book widget iframe to browser edge

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<div id="ttr_sidebar" class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+<div id="ttr_sidebar" class="col-lg-2 col-md-2 col-sm-4 col-xs-12">
     <h2 ><?php _e('Categories'); ?></h2>
     <ul > <?php wp_list_categories(['orderby' => 'name', 'title_li' => '']); ?> </ul>
     <h2 ><?php _e('Archives'); ?></h2>
