bc65c104927843b26b9e74aa74d929fb94c6d7e2
Mike Lindner <mikelindner@mail.com>
2026-02-02 14:18:43 +1030

    Replace Leanpub widget with WooCommerce random product display

--- sidebar.php
+++ sidebar.php
@@ -4,9 +4,41 @@
     <h2 ><?php _e('Archives'); ?></h2>
     <ul > <?php wp_get_archives(); ?> </ul>
 
-    <div class="book-widget">
-        <a href="https://leanpub.com/CloudBook" target="_blank" rel="noopener"></a>
-        <iframe width='280' height='400' src='https://leanpub.com/CloudBook/embed' frameborder='0' allowtransparency='true'></iframe>
+    <?php if (class_exists('WooCommerce')) :
+        $args = [
+            'post_type' => 'product',
+            'posts_per_page' => 1,
+            'orderby' => 'rand',
+            'post_status' => 'publish',
+        ];
+        $random_product = new WP_Query($args);
+        if ($random_product->have_posts()) :
+            while ($random_product->have_posts()) : $random_product->the_post();
+                global $product;
+    ?>
+    <div class="store-widget">
+        <a href="<?php the_permalink(); ?>" class="store-widget-link">
+            <div class="store-widget-image">
+                <?php if (has_post_thumbnail()) :
+                    the_post_thumbnail('medium');
+                else : ?>
+                    <div class="no-image">No Image</div>
+                <?php endif; ?>
+            </div>
+            <div class="store-widget-info">
+                <h3><?php the_title(); ?></h3>
+                <?php if ($product) : ?>
+                    <span class="price"><?php echo $product->get_price_html(); ?></span>
+                <?php endif; ?>
+                <span class="shop-now">Shop Now →</span>
+            </div>
+        </a>
     </div>
+    <?php
+            endwhile;
+            wp_reset_postdata();
+        endif;
+    endif;
+    ?>
   
 </div>
